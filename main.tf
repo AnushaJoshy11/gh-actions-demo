65cb36e693dff6c8f75c29fd297740a9abb0a6fa
AnushaJoshy11 <76212672+AnushaJoshy11@users.noreply.github.com>
2021-11-22 11:13:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,64 +2,55 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.66.0"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
     }
   }
+  required_version = ">= 0.14"
 
-  required_version = ">= 0.14.9"
+  backend "remote" {
+    organization = "REPLACE_ME"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
 }
+
 
 provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
+  region = "us-west-2"
 }
 
 
-terraform {
- backend "remote" {
-   organization = "TestAnushaOrg"
 
-   workspaces {
-     name = "gh-actions-demo"
-   }
- }
+resource "random_pet" "sg" {}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-830c94e3"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 }
 
-# module "ec2_instance" {
-#   source  = "terraform-aws-modules/ec2-instance/aws"
-#   version = "~> 3.0"
-
-#   name          = var.ec2_name
-#   ami           = "ami-0416f96ae3d1a3f29"
-#   instance_type = "t2.micro"
-#   key_name      = "anushakeypair"
-#   monitoring    = true
-#   #vpc_security_group_ids = [module.vpc.default_security_group_id]
-#   vpc_security_group_ids = var.security_group_ids
-#   subnet_id              = var.subnet_id
-#   availability_zone = var.availability_zone
-
-#   tags =  {
-#     Terraform   = "true"
-#     Environment = "dev" 
-#   }
-# }
-
-  module "sns_topic" {
-    source  = "terraform-aws-modules/sns/aws"
-    version = "~> 3.0"
-
-    name = "myterraformsns-topic"
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  module "s3_bucket" {
-    source = "terraform-aws-modules/s3-bucket/aws"
-
-    bucket = "my-s3-bucket"
-    acl    = "private"
-
-    versioning = {
-      enabled = true
-    }
-
-  }
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
