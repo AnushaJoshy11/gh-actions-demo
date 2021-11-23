bb7bd8249ade92b91abc33c11f2facd9ed76114b
AnushaJoshy11 <anusha.joshell@gmail.com>
2021-11-23 12:50:31 +0000

    port80810

--- main.tf
+++ main.tf
@@ -45,8 +45,8 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 808
-    to_port     = 808
+    from_port   = 80810
+    to_port     = 80810
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
